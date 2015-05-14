6bfc05f35b2145b12609651c70ccd8b428275232
Noah Levine <nclevine@gmail.com>
2015-05-14 01:57:37 -0400

    nevermind

--- app/assets/javascripts/artwork_search_met.js
+++ app/assets/javascripts/artwork_search_met.js
@@ -1,5 +1,5 @@
 function metCollectionSearch(searchTerm){
-  var searchURL = '//scrapi.org/search/' + searchTerm;
+  var searchURL = 'http://scrapi.org/search/' + searchTerm;
   $.ajax({
     method: 'get',
     dataType: 'json',
@@ -32,7 +32,7 @@ function getMetResults(response){
 };
 
 function metItemLookup(metId){
-  var lookupURL = "//scrapi.org/object/" + metId;
+  var lookupURL = "http://scrapi.org/object/" + metId;
   $.ajax({
     method: 'get',
     dataType: 'json',
